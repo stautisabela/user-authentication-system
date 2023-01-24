49c259ba58ac6828d59b4835607fba89a22cfa55
stautisabela <stautisabela@gmail.com>
2023-01-24 15:01:31 +0100

    first commit

---
+++ user-authentication-system/src/main/java/com/stautisabela/userauthenticationsystem/UserAuthenticationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.stautisabela.userauthenticationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationSystemApplication.class, args);
+	}
+
+}
---
+++ user-authentication-system/src/test/java/com/stautisabela/userauthenticationsystem/UserAuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stautisabela.userauthenticationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
