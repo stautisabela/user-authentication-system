cb27d134bda8b7a28820b5f960f97ffa28be1f52
stautisabela <stautisabela@gmail.com>
2023-01-25 18:15:41 +0100

    createdd SecurityConfig class

--- user-authentication-system/src/main/java/com/stautisabela/userauthenticationsystem/security/JwtConfigurer.java
+++ user-authentication-system/src/main/java/com/stautisabela/userauthenticationsystem/security/JwtConfigurer.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import jakarta.servlet.Filter;
 
 public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
